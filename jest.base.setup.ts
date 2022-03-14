3270066552b802a2f50ab9b04e4a947e15dcc09d
felipejhordan <felipejordan.alves@gmail.com>
2022-03-14 00:16:54 -0300

    chore: separate main jest config of package.json and create jest configs like as typescript files

---
+++ jest.base.setup.ts
@@ -0,0 +1,13 @@
+export const jestBaseConfig = {
+  verbose: true,
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1',
+  },
+};
