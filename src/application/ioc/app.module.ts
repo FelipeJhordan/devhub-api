be1f53ba5a896e912dbd9fda6dafcc8785c0350f
felipejhordan <felipejordan.alves@gmail.com>
2022-03-15 22:41:36 -0300

    feat: import health module and prisma module in app module

--- src/application/ioc/app.module.ts
+++ src/application/ioc/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppService } from '@/application/services/app.service';
 import { AppController } from '@/presentation/controllers/app.controller';
+import { HealthModule } from './health.module';
+import { PrismaModule } from './prisma.module';
 
 @Module({
-  imports: [],
+  imports: [HealthModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
