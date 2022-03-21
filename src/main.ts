e9d78789514dad69a4a974d220147186f4e0e2ca
felipejhordan <felipejordan.alves@gmail.com>
2022-03-21 00:13:35 -0300

    feat: set cors and set middlewares

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '@/application/ioc/app.module';
+import { MiddlewaresComposite } from './application/middlewares/middlewares.composite';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+  });
+
+  new MiddlewaresComposite().apply(app.use);
+
   await app.listen(3333);
 }
 bootstrap();
