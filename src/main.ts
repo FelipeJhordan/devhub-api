f07e72fbe46b285e40dd3a698105c9f725b410e0
felipejhordan <felipejordan.alves@gmail.com>
2022-04-05 16:44:55 -0300

    feat: define filters, pipes and interceptors in main file

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,36 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from '@/application/ioc/app.module';
-import { MiddlewaresComposite } from './application/middlewares/middlewares.composite';
+import { Logger, ValidationPipe } from '@nestjs/common';
+
+import chalk from 'chalk';
 import { ConfigService } from '@nestjs/config';
 
+import { MiddlewaresComposite } from './application/middlewares/middlewares.composite';
+import { AppModule } from '@/application/ioc/app.module';
+import { HttpExceptionFilter } from './infra/rest/http-exception.filter';
+import { LoggingInterceptor } from './infra/rest/logging.interceptor';
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    cors: true,
-  });
+  try {
+    const app = await NestFactory.create(AppModule, {
+      cors: true,
+    });
 
-  const configService = app.get(ConfigService);
-  const port = configService.get('API_PORT');
+    app.useGlobalFilters(new HttpExceptionFilter());
+    app.useGlobalPipes(new ValidationPipe());
+    app.useGlobalInterceptors(new LoggingInterceptor());
 
-  new MiddlewaresComposite().apply(app.use);
+    const configService = app.get(ConfigService);
 
-  await app.listen(port);
+    const port = configService.get('API_PORT');
+
+    new MiddlewaresComposite().apply(app.use);
+
+    await app.listen(port);
+
+    Logger.log(`🚀 Server is listening on port ${chalk.hex('#aabbff').bold(`${port}`)}`);
+  } catch (e) {
+    Logger.error('🛑 Error to the starting server, ' + e, 'Bootstrap');
+    process.exit();
+  }
 }
-bootstrap().catch((err) => console.log('Failed to start error' + err)); // Vou melhorar isso depois.
+bootstrap();
