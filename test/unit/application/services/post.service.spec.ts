369730e49e05ddb47a26db0295305aea6911cb9a
JVBorges <joao-salves@hotmail.com>
2022-04-10 15:11:13 -0300

    refactor: adopt usage of postStub in post service unit tests

--- test/unit/application/services/post.service.spec.ts
+++ test/unit/application/services/post.service.spec.ts
@@ -1,6 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PostService } from '@/application/services/post.service';
 import { PrismaService } from '@/application/services/prisma.service';
+import { PrismaService as mockedPrisma } from '../../mocks/prisma/prisma.service';
+import { postStub } from '../../stubs/post/post.stub';
 
 describe('<PostService>', () => {
   let service: PostService;
@@ -8,7 +10,13 @@ describe('<PostService>', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PostService, PrismaService],
+      providers: [
+        PostService,
+        {
+          provide: PrismaService,
+          useValue: mockedPrisma,
+        },
+      ],
     }).compile();
 
     service = module.get<PostService>(PostService);
@@ -18,87 +26,54 @@ describe('<PostService>', () => {
 
   describe('createPost', () => {
     it('should be able to create a new post', async () => {
-      const post = {
-        id: 1,
-        content: 'Create post unit test',
-        created_at: new Date(),
-        user_id: 1,
-      };
-
-      prisma.post.create = jest.fn().mockReturnValue(post);
+      prisma.post.create = jest.fn().mockResolvedValue(postStub());
 
       const result = await service.createPost({
-        content: 'Create post unit test',
-        created_at: new Date().toISOString(),
-        user_id: 1,
+        content: postStub().content,
+        user_id: postStub().user_id,
       });
 
-      expect(result).toEqual(post);
+      expect(result).toEqual(postStub());
     });
   });
 
   describe('getUserPosts', () => {
     it('should be able to list all posts from certain user', async () => {
-      const posts = [
-        {
-          id: 1,
-          content: 'First post from user 1',
-          created_at: new Date(),
-          user_id: 1,
-        },
-      ];
+      prisma.post.findMany = jest.fn().mockResolvedValue([postStub()]);
 
-      prisma.post.findMany = jest.fn().mockReturnValue(posts);
+      const result = await service.getUserPosts({ user_id: postStub().user_id });
 
-      const result = await service.getUserPosts({ user_id: 1 });
-
-      expect(result).toEqual(posts);
+      expect(result).toEqual([postStub()]);
     });
   });
 
   describe('updatePost', () => {
     it('should be able to update a post', async () => {
-      const post = {
-        id: 1,
-        content: 'updated post',
-        created_at: new Date(),
-        user_id: 1,
-      };
+      prisma.post.update = jest.fn().mockResolvedValue(postStub());
 
-      prisma.post.update = jest.fn().mockReturnValue(post);
+      const result = await service.updatePost({ post_id: postStub().id, updated_content: postStub().content });
 
-      const result = await service.updatePost({ post_id: 1, updated_content: 'updated post' });
-
-      expect(result).toEqual(post);
+      expect(result).toEqual(postStub());
     });
   });
 
   describe('getPost', () => {
     it('should be able to fetch a single post', async () => {
-      const post = {
-        id: 1,
-        content: 'First post from user 1',
-        created_at: new Date(),
-        user_id: 1,
-      };
+      prisma.post.findUnique = jest.fn().mockResolvedValue(postStub());
 
-      prisma.post.findUnique = jest.fn().mockReturnValue(post);
+      const result = await service.getPost({ post_id: postStub().id });
 
-      const result = await service.getPost({ post_id: 1 });
-
-      expect(result).toEqual(post);
+      expect(result).toEqual(postStub());
     });
   });
 
   describe('deletePosts', () => {
     it('should be able to delete a single or multiple posts', async () => {
-      const posts_id = [1, 2, 3];
+      prisma.post.deleteMany = jest.fn().mockResolvedValue(1);
 
-      prisma.post.deleteMany = jest.fn().mockReturnValue(3);
+      const result = await service.deletePosts({ posts_id: [postStub().id] });
 
-      const result = await service.deletePosts({ posts_id });
-
-      expect(result).toEqual(posts_id.length);
+      expect(result).toEqual(1);
     });
   });
 });
