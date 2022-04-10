4cf67ce11a316d15e001dba5a86b4cf5b6f30f7f
JVBorges <joao-salves@hotmail.com>
2022-04-10 15:39:56 -0300

    feat: create PostModule and add to the AppModule

--- src/application/ioc/app.module.ts
+++ src/application/ioc/app.module.ts
@@ -4,9 +4,10 @@ import { AppController } from '@/presentation/controllers/app.controller';
 import { HealthModule } from './health.module';
 import { PrismaModule } from './prisma.module';
 import { ConfigModule } from '@nestjs/config';
+import { PostModule } from './post.module';
 
 @Module({
-  imports: [HealthModule, PrismaModule, ConfigModule.forRoot()],
+  imports: [HealthModule, PrismaModule, PostModule, ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
