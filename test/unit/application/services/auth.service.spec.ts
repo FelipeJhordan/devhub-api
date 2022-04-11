e3d1f6e1c90aab31eb15ecb7441700cca92e44d3
felipejhordan <felipejordan.alves@gmail.com>
2022-04-11 17:40:52 -0300

    test: create auth service

--- test/unit/application/services/auth.service.spec.ts
+++ test/unit/application/services/auth.service.spec.ts
@@ -1,12 +1,25 @@
-// import { Test, TestingModule } from "@nestjs/testing";
+import { AuthService } from '@/application/services/auth.service';
+import { PrismaService } from '@/application/services/prisma.service';
+import { Test, TestingModule } from '@nestjs/testing';
+import { Prisma } from '@prisma/client';
+import { PrismaService as mockedPrisma } from '../../mocks/prisma/prisma.service';
 
-// describe('<AuthService>', () => {
+describe('AuthService', () => {
+  let service: AuthService;
+  let prisma: PrismaService;
 
-//     beforeEach(async() => {
-//         const module: TestingModule = await Test.createTestingModule({
-//             providers: [
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        AuthService,
+        {
+          provide: PrismaService,
+          useValue: mockedPrisma,
+        },
+      ],
+    }).compile();
 
-//             ]
-//         })
-//     })
-// });
+    service = module.get<AuthService>(AuthService);
+    prisma = module.get<PrismaService>(PrismaService);
+  });
+});
