3c869ff9b6086247deb8c8d45d4cee58b5411417
felipejhordan <felipejordan.alves@gmail.com>
2022-04-12 21:39:50 -0300

    feat: configure jwtModule and PassportModule

--- src/application/ioc/auth.module.ts
+++ src/application/ioc/auth.module.ts
@@ -1,10 +1,26 @@
+import { HashAdapterImp } from '@/infra/hashing/hash.adapter.imp';
+import { JwtCustomConfig } from '@/infra/jwt/config/jwt.custom.config';
+import { JwtStrategy } from '@/infra/passport/jwt-strategy/jwt.strategy';
 import { AuthController } from '@/presentation/controllers/auth.controller';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { AuthService } from '../services/auth.service';
-
+import { HashingAdapter } from '../services/protocols/hashing.adapter';
+import { SessionModule } from './session.module';
+import { UserModule } from './user.module';
 @Module({
-  imports: [],
+  imports: [UserModule, SessionModule, JwtModule.registerAsync({ imports: [ConfigModule], useClass: JwtCustomConfig })],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [
+    PassportModule,
+    AuthService,
+    {
+      provide: HashingAdapter,
+      useClass: HashAdapterImp,
+    },
+    JwtStrategy,
+  ],
 })
 export class AuthModule {}
