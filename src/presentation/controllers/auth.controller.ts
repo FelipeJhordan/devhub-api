2a0a872bf1bdc8d15cd4ab4abc4d5ccd96d36f48
felipejhordan <felipejordan.alves@gmail.com>
2022-04-12 21:38:10 -0300

    feat: add logout endpoint

--- src/presentation/controllers/auth.controller.ts
+++ src/presentation/controllers/auth.controller.ts
@@ -1,12 +1,19 @@
+import { Body, Controller, Delete, HttpCode, HttpStatus, Post, UseGuards } from '@nestjs/common';
+
 import { AuthService } from '@/application/services/auth.service';
-import { Body, Controller, Post } from '@nestjs/common';
-import { LoginUserDto } from '../dtos/auth/loginUser.dto';
-import { RegisterUserDto } from '../dtos/auth/registerUser.dto';
+import { SessionService } from '@/application/services/session.service';
+import { IJwtPayload } from '@/infra/jwt/protocol/jwt.payload.protocol';
+import { JwtAuthGuard } from '@/application/guards/jwt.auth.guard';
+import { UserDecorator } from '../decorators/user.decorator';
+
+import { LoginUserDto } from '../dtos/auth/LoginUser.dto';
+import { RegisterUserDto } from '../dtos/auth/RegisterUser.dto';
 import { AuthUserResponseDto } from '../dtos/auth/AuthUserResponse.dto';
+import { MessageDto } from '../dtos/shared/message.dto';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private sessionService: SessionService) {}
 
   @Post('register')
   public async registerUser(@Body() registerUserDto: RegisterUserDto): Promise<AuthUserResponseDto> {
@@ -16,5 +23,14 @@ export class AuthController {
   @Post('login')
   public async loginUser(@Body() loginUserDto: LoginUserDto): Promise<AuthUserResponseDto> {
     return await this.authService.login(loginUserDto);
+  }
+
+  @Delete('logout')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @UseGuards(JwtAuthGuard)
+  public async logout(@UserDecorator() { userId }: IJwtPayload): Promise<MessageDto> {
+    await this.sessionService.destroySession(userId);
+
+    return MessageDto.createMessage('Logout success', 204);
   }
 }
