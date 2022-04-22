749b3c159c26118375157996535d937d52f264e8
Felipe Jhordan <44248690+FelipeJhordan@users.noreply.github.com>
2022-04-22 10:32:17 -0300

    refactor: remove ghost github file

--- src/presentation/dtos/user/CreateUser.dto.ts
+++
@@ -1,9 +0,0 @@
-import { FavoriteLanguageEnum } from '../../../domain/enum/FavoriteLanguage.enum';
-
-export class CreateUserDto {
-  name: string;
-  email: string;
-  password: string;
-  birth: Date;
-  language: FavoriteLanguageEnum[];
-}
