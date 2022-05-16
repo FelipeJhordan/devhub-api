c147195e19dc9000e5fdb404fda100624753e567
JVBorges <joao-salves@hotmail.com>
2022-05-15 22:11:27 -0300

    feat: add comment validator to comment module

--- src/application/ioc/comment.module.ts
+++ src/application/ioc/comment.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 
 import { CommentController } from '@/presentation/controllers/comment.controller';
 import { CommentService } from '../services/comment.service';
+import { CommentExists } from '@/infra/class-validator/comment-exists.validator';
 
 @Module({
   controllers: [CommentController],
-  providers: [CommentService],
+  providers: [CommentService, CommentExists],
 })
 export class CommentModule {}
