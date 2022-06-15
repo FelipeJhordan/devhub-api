05ab7de8182eeb2a523b82ac6fdb6e98647cba41
felipejhordan <felipejordan.alves@gmail.com>
2022-06-15 18:33:25 -0300

    feat: create controller/service for unfollow other user/profile

--- src/presentation/controllers/follow.controller.ts
+++ src/presentation/controllers/follow.controller.ts
@@ -1,6 +1,6 @@
 import { JwtAuthGuard } from '@/application/guards/jwt.auth.guard';
 import { FollowService } from '@/application/services/follow.service';
-import { Controller, Param, Post, Req, UseGuards } from '@nestjs/common';
+import { Controller, Delete, Param, Post, Req, UseGuards } from '@nestjs/common';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { FollowDTO } from '../dtos/follow/follow.dto';
 
@@ -14,6 +14,14 @@ export class FollowController {
   @Post(':id')
   async followUser(@Req() { user }, @Param() { id }: FollowDTO) {
     return this.followService.create({
+      id,
+      followee: user.userId,
+    });
+  }
+
+  @Delete(':id')
+  async unfollowUser(@Req() { user }, @Param() { id }: FollowDTO): Promise<void> {
+    return this.followService.delete({
       id,
       followee: user.userId,
     });
